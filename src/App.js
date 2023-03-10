df2c469de627e6612e873b3b15aa3808c92a73af
Akp1741 <110102619+Akp1741@users.noreply.github.com>
2023-03-10 17:49:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import RegistrationForm from './components/registrationForm'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
