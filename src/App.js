0e010c2b8f214a5fc808af79045baf4ca0987faa
AyushPatel <110102619+Akp1741@users.noreply.github.com>
2023-05-25 10:52:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import { Component } from 'react';
-
+import React from 'react';
+import logo from "./logo.svg";
 import './App.css';
 import Greet from './components/Greet';
-import Form from './components/Form';
+import Registration from "./components/Registration";
+import LoginForm from "./components/LoginForm";
+import CounterHook from "./components/CounterHook";
+import LoginHook from "./components/LoginHook";
+import { useState } from "react";
+import { Button } from "@mui/material";
+
 import About from './components/About';
 import Home from './components/Home';
 
 import{BrowserRouter as Router,Route,Link} from 'react-router-dom'
 
-class App extends React.Component{
-  render(){
+
+function App() {
+  const [data, setData] = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <Registration></Registration>
+      <br></br>
+      <br></br>
+      <br></br>
+    
+      </div> 
+        );
+      }
+      {/*
+      <CounterHook data={data} count={count}></CounterHook>
+      <Button
+        variant="contained"
+        color="error"
+        onClick={() => setData(data + 1)}
+      >
+        Update Data
+      </Button>
+      <br></br>
+      <Button
+        variant="contained"
+        color="warning"
+        onClick={() => setCount(count + 1)}
+      >
+        Update Count
+      </Button>
+  
+      <LoginHook></LoginHook>
+    </div>
+  );
+}*/}
+
+/*lass App extends React.Component
+{
+  render()
+  {
+   
+    return (
+    <div className="App">
      <Greet/>
-     <Welcome/>
+      <Welcome/>
+     <LoginForm/>
   <Router>
   <Link to="">Home</Link>
   <Link to="/about">About</Link>
@@ -22,16 +69,18 @@ class App extends React.Component{
   <Route exaxt path="/" Component={Home}/>
   <Route path="/About" Component={About}/>
   <Route path="/Form" Component={Form}/>
-  s
+
       {/*<About/>
       <Home/>
   <Form/>
-  */}
+ 
   </Router>
   </div>
     
   );
 }
-}
+}*/
 
 export default App;
+
+
